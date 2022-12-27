960b3ec4652d35d29b3c66acb0d009a2e3839554
safiullahrafeeq77777 <safiullahrafeeq7777@gmail.com>
2022-12-27 18:51:39 +0500

    changes

--- components/metaHead/MetaHead.jsx
+++ components/metaHead/MetaHead.jsx
@@ -2,6 +2,20 @@ import Head from "next/head";
 import React from "react";
 
 const MetaHead = ({ title, description, link }) => {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Corporation",
+    "name": "Creaditech",
+    "url": "https://creaditech.com/",
+    "logo": "https://creaditech.com/_next/static/media/Logo.a9789352.svg",
+    "sameAs": [
+      "https://www.facebook.com/creaditech",
+      "https://twitter.com/Creaditech",
+      "https://www.instagram.com/creaditech/",
+      "https://www.linkedin.com/company/creaditechus/",
+      "https://www.pinterest.com/Creaditech/"
+    ]
+  };
   return (
     <Head>
       <meta
@@ -17,6 +31,12 @@ const MetaHead = ({ title, description, link }) => {
         }
       />
       <link rel="canonical" href={`https://creaditech.com/${link || ""}`} />
+      
+      <script
+        key="structured-data"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
     </Head>
   );
 };
